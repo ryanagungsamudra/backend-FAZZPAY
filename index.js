66f54ba094e9ec2c9e190bb21f54a31f6c1deaeb
ryanagungsamudra <ryansamudra67@gmail.com>
2023-03-10 00:14:35 +0700

    fix: fix heroku configuration

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("*", (req, res) => {
   });
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, (req, res) => {
   console.log(`Fazzpay server running on port ${port}`);
 });
